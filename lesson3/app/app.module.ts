22b1a6f62c2d68de244e90a849059e7c8b360cb7
Marcello <mgr@MacBook-Pro-de-Marcello.local>
2016-08-18 23:06:23 +1200

    separeted components and created services

--- lesson3/app/app.module.ts
+++ lesson3/app/app.module.ts
@@ -2,17 +2,29 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+import { routing } from './app.routing';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
   ],
-  bootstrap: [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ 
+    AppComponent
+  ]
 })
 export class AppModule { }
