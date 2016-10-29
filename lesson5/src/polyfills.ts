944bd922dc84b90ef50daf1264a2284d72a5661b
Marcello Rego <marcellorego@gmail.com>
2016-10-29 19:38:29 +1300

    Added correct typings dependencies

--- lesson5/src/polyfills.ts
+++ lesson5/src/polyfills.ts
@@ -1,16 +1,16 @@
-import 'core-js';
+import 'core-js/client/shim';
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 
 import 'ts-helpers';
 
-//if (process.env.ENV === 'build') {
+if (process.env.ENV === 'build') {
   // Production
 
-//} else {
+} else {
   // Development
 
   Error['stackTraceLimit'] = Infinity;
 
-  //require('zone.js/dist/long-stack-trace-zone');
-//}
+  require('zone.js/dist/long-stack-trace-zone');
+}
